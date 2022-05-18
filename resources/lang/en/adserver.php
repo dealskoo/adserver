be345e40cd323d9891fb669708ac13d0dfec793c
jabber <2898117012@qq.com>
2022-05-18 18:33:35 +0800

    init

--- resources/lang/en/adserver.php
+++ resources/lang/en/adserver.php
@@ -2,5 +2,13 @@
 return [
     'adserver' => 'Ad Server',
     'ads' => 'Ads',
-    'ad_spaces' => 'Ad Spaces'
+
+    'ad_spaces' => 'Ad Spaces',
+    'ad_spaces_list' => 'Ad Spaces List',
+    'add_ad_space' => 'Add Ad Space',
+    'id' => 'ID',
+    'code' => 'Code',
+    'created_at' => 'Created At',
+    'updated_at' => 'Updated At',
+    'action' => 'Action'
 ];
